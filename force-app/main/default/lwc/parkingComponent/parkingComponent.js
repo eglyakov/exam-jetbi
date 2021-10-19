6da4b720ccfb0b1019b71d9edc0925fa403710a6
Egor Glyakov <eglyakov@gmail.com>
2021-10-19 23:57:29 +0300

    feat: implement uploading file

--- force-app/main/default/lwc/parkingComponent/parkingComponent.js
+++ force-app/main/default/lwc/parkingComponent/parkingComponent.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ParkingComponent extends LightningElement {}
+const COLUMS = [
+  {label: "Sensor model", fieldName: "Sensor_model__c"},
+  {label: "Status", fieldName: "Status__c"},
+  {label: "Base Station", fieldName: "Base_Station__c"},
+];
+
+export default class ParkingComponent extends LightningElement {
+  @api
+  recordId;
+
+  get acceptedFormats() {
+    return ['.csv'];
+  }
+
+  handleUploadFinished(event) {
+    const uploadedFiles = event.detail.files;
+    alert('No. of files uploaded : ' + uploadedFiles.length);
+  }
+}
