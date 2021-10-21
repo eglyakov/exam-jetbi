634232f7a9fb9403f8abab6ec63a6d688451554b
Egor Glyakov <eglyakov@gmail.com>
2021-10-21 22:17:48 +0300

    feat: implement adding in DB

--- force-app/main/default/lwc/parkingComponent/parkingComponent.js
+++ force-app/main/default/lwc/parkingComponent/parkingComponent.js
@@ -1,14 +1,22 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
+import readCSVFile from '@salesforce/apex/ParkingController.readCSVFile';
 
-const COLUMS = [
-  {label: "Sensor model", fieldName: "Sensor_model__c"},
-  {label: "Status", fieldName: "Status__c"},
-  {label: "Base Station", fieldName: "Base_Station__c"},
+const COLUMNS = [
+  {label: "Sensor model", fieldName: "SensorModel"},
+  {label: "Status", fieldName: "Status"},
+  {label: "Base Station", fieldName: "BaseStation"},
+  {
+    type: 'action',
+    typeAttributes: { rowActions: 'delete' },
+  },
 ];
 
 export default class ParkingComponent extends LightningElement {
-  @api
-  recordId;
+  @api recordId;
+  @track data;
+  @track error;
+  @track columns = COLUMNS;
+  // record = {};
 
   get acceptedFormats() {
     return ['.csv'];
@@ -16,6 +24,95 @@ export default class ParkingComponent extends LightningElement {
 
   handleUploadFinished(event) {
     const uploadedFiles = event.detail.files;
-    alert('No. of files uploaded : ' + uploadedFiles.length);
+    console.log(uploadedFiles[0].documentId)
+    readCSVFile({contentDocumentId: uploadedFiles[0].documentId})
+    .then(result => {
+      console.log('result ===> '+result);
+      this.data = result;
+      console.log('this.data ===> '+this.data);
+      console.log('data ===> '+data);
+    })
+    .catch(err => {
+      this.error = err;
+      console.log('err ===> '+err);
+    })
+
+  }
+
+  deleteRow(event) {
+    const id = event.detail.row;
+    const index = this.findRowIndexById(id);
+    if (index !== -1) {
+      this.data = this.data
+        .slice(0, index)
+        .concat(this.data.slice(index + 1));
+    }
+  }
+
+  findRowIndexById(id) {
+    let ret = -1;
+    this.data.some((row, index) => {
+      if (row.id === id) {
+        ret = index;
+        return true;
+      }
+      return false;
+    });
+    return ret;
   }
 }
+
+// const actions = [
+//     { label: 'Show details', name: 'show_details' },
+//     { label: 'Delete', name: 'delete' },
+// ];
+
+
+// export default class DatatableWithRowActions extends LightningElement {
+    
+
+//     // eslint-disable-next-line @lwc/lwc/no-async-await
+//     // async connectedCallback() {
+//     //    this.data = await fetchDataHelper({ amountOfRecords: 100 });
+//     // }
+
+//     handleRowAction(event) {
+//         const actionName = event.detail.action.name;
+//         const row = event.detail.row;
+//         switch (actionName) {
+//             case 'delete':
+//                 this.deleteRow(row);
+//                 break;
+//             case 'show_details':
+//                 this.showRowDetails(row);
+//                 break;
+//             default:
+//         }
+//     }
+
+//     deleteRow(event) {
+//       const id = event.detail.row;
+//       const index = this.findRowIndexById(id);
+//       if (index !== -1) {
+//         this.data = this.data
+//           .slice(0, index)
+//           .concat(this.data.slice(index + 1));
+//       }
+//     }
+
+//     findRowIndexById(id) {
+//       let ret = -1;
+//       this.data.some((row, index) => {
+//         if (row.id === id) {
+//           ret = index;
+//           return true;
+//         }
+//         return false;
+//       });
+//       return ret;
+//     }
+
+//     showRowDetails(row) {
+//         this.record = row;
+//     }
+// }
