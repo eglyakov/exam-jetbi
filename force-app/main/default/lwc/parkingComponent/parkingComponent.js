064e6c00f020814c904bc861f9a2022e2374c16d
Egor Glyakov <eglyakov@gmail.com>
2021-10-22 12:40:41 +0300

    fix: chang name sensor mofel

--- force-app/main/default/lwc/parkingComponent/parkingComponent.js
+++ force-app/main/default/lwc/parkingComponent/parkingComponent.js
@@ -2,7 +2,7 @@ import { LightningElement, track, api } from 'lwc';
 import readCSVFile from '@salesforce/apex/ParkingController.readCSVFile';
 
 const COLUMNS = [
-  {label: "Sensor model", fieldName: "SensorModel"},
+  {label: "Sensor Model", fieldName: "SensorModel"},
   {label: "Status", fieldName: "Status"},
   {label: "Base Station", fieldName: "BaseStation"},
   {
@@ -16,7 +16,6 @@ export default class ParkingComponent extends LightningElement {
   @track data;
   @track error;
   @track columns = COLUMNS;
-  // record = {};
 
   get acceptedFormats() {
     return ['.csv'];
@@ -24,19 +23,17 @@ export default class ParkingComponent extends LightningElement {
 
   handleUploadFinished(event) {
     const uploadedFiles = event.detail.files;
-    console.log(uploadedFiles[0].documentId)
     readCSVFile({contentDocumentId: uploadedFiles[0].documentId})
     .then(result => {
-      console.log('result ===> '+result);
+      console.log(result);
+      console.log(this.data);
       this.data = result;
-      console.log('this.data ===> '+this.data);
-      console.log('data ===> '+data);
+      console.log(this.data);
     })
     .catch(err => {
       this.error = err;
-      console.log('err ===> '+err);
     })
-
+    
   }
 
   deleteRow(event) {
@@ -61,58 +58,3 @@ export default class ParkingComponent extends LightningElement {
     return ret;
   }
 }
-
-// const actions = [
-//     { label: 'Show details', name: 'show_details' },
-//     { label: 'Delete', name: 'delete' },
-// ];
-
-
-// export default class DatatableWithRowActions extends LightningElement {
-    
-
-//     // eslint-disable-next-line @lwc/lwc/no-async-await
-//     // async connectedCallback() {
-//     //    this.data = await fetchDataHelper({ amountOfRecords: 100 });
-//     // }
-
-//     handleRowAction(event) {
-//         const actionName = event.detail.action.name;
-//         const row = event.detail.row;
-//         switch (actionName) {
-//             case 'delete':
-//                 this.deleteRow(row);
-//                 break;
-//             case 'show_details':
-//                 this.showRowDetails(row);
-//                 break;
-//             default:
-//         }
-//     }
-
-//     deleteRow(event) {
-//       const id = event.detail.row;
-//       const index = this.findRowIndexById(id);
-//       if (index !== -1) {
-//         this.data = this.data
-//           .slice(0, index)
-//           .concat(this.data.slice(index + 1));
-//       }
-//     }
-
-//     findRowIndexById(id) {
-//       let ret = -1;
-//       this.data.some((row, index) => {
-//         if (row.id === id) {
-//           ret = index;
-//           return true;
-//         }
-//         return false;
-//       });
-//       return ret;
-//     }
-
-//     showRowDetails(row) {
-//         this.record = row;
-//     }
-// }
